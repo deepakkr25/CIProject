62c9d0fafd0c1a953e29385a5a92d2770d8c91be
Deepak <deepak@Deepaks-Mac-mini.local>
2018-08-10 12:53:29 +0530

    First commit

---
+++ CIProject/CIProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIProject
+//
+//  Created by Deepak on 10/08/18.
+//  Copyright © 2018 Deepak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
