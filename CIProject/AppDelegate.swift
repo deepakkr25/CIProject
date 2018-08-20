7d56699d66acfe4144d9df3ea63c7f65815cacfe
Deepak <deepak@Deepaks-Mac-mini.local>
2018-08-20 15:26:49 +0530

    updated Appdelegate

--- CIProject/AppDelegate.swift
+++ CIProject/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let window = self.window
+        let vc = window?.rootViewController
+        let vc1 = window?.rootViewController
         return true
     }
 
